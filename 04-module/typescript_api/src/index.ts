4eb9663799dc803aa634dd9a0ac4863f3477cd12
git <gula16@itu.edu.tr>
2025-10-28 15:51:54 +0300

    Add Dockerfile for typescript_api

--- 04-module/typescript_api/src/index.ts
+++ 04-module/typescript_api/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 
 const app = express();
+const appName = process.env.APP_NAME || "typescript_api";
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => res.send('Hello world from typescript_api'));
+app.get('/', (req, res) => {
+  const timestamp = new Date().toISOString(); // e.g. 2025-10-21T14:30:00.123Z
+  res.send(`Hello world from express_api. The app name is: ${appName}. Current time is: ${timestamp}`)
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
