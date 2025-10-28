d5ed4e4c33c88ae2908832b691eeeda9f5cde41e
git <gula16@itu.edu.tr>
2025-10-28 12:45:23 +0300

    typescript-api source code

---
+++ 04-module/typescript_api/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) => res.send('Hello world from typescript_api'));
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
