99377c34e674192bb33c6cdd110db3437edf867b
git <gula16@itu.edu.tr>
2025-10-29 11:42:08 +0300

    Add ALLOWED_ORIGINS for CORS

--- 04-module/typescript_api/src/index.ts
+++ 04-module/typescript_api/src/index.ts
@@ -4,9 +4,33 @@ import cors from "cors";
 const app = express();
 const appName = process.env.APP_NAME || "typescript_api";
 const port = process.env.PORT || 3000;
+const allowedOriginsEnv = process.env.ALLOWED_ORIGINS || "http://localhost:5173";
+const allowedOrigins = allowedOriginsEnv.split(",");
 
+/*
 // Enable CORS for all routes
-app.use(cors());
+//app.use(cors());
+
+// Allowed origins
+const allowedOrigins = [
+  "http://localhost:5173"
+];
+*/
+
+// Configure CORS
+app.use(cors({
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);  // allow
+    } else {
+      callback(new Error("Not allowed by CORS"));  // block
+    }
+  },
+  optionsSuccessStatus: 200
+}));
 
 app.get('/', (req, res) => {
   const timestamp = new Date().toISOString(); // e.g. 2025-10-21T14:30:00.123Z
