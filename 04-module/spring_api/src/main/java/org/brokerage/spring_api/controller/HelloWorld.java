0abb35fe907ae6ab01d39086f8eb5e5ede6d3598
git <gula16@itu.edu.tr>
2025-11-19 18:00:16 +0300

    CORS added

--- 04-module/spring_api/src/main/java/org/brokerage/spring_api/controller/HelloWorld.java
+++ 04-module/spring_api/src/main/java/org/brokerage/spring_api/controller/HelloWorld.java
@@ -4,12 +4,14 @@ import org.brokerage.spring_api.config.AppConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class HelloWorld {
 
     //@Value("${app.name}")
