f44714c06d8f87161f945ac3e689d7476bbf6ba3
git <gula16@itu.edu.tr>
2025-11-23 17:16:27 +0300

    Update notebook service call, add env value to compose.override.yaml file

--- 06-module/notes-backend/src/infrastructure/services/notebook-service.js
+++ 06-module/notes-backend/src/infrastructure/services/notebook-service.js
@@ -1,9 +1,20 @@
 import { ServiceUnavailableError } from '../../domain/errors.js';
+import axios from 'axios';
 
 export class HttpNotebookService {
   // In a real app, you would inject 'axios' or 'fetch' here
   async checkExists(notebookId) {
     console.log(`Checking existence of Notebook ID: ${notebookId}...`);
+
+    try{
+      const API_URL = process.env.NOTEBOOKS_API_URL;
+      console.log(`Calling service: ${API_URL}/${notebookId}`)
+      await axios.get(`${API_URL}/${notebookId}`)
+
+    }catch(err){
+      console.error(`Notebook not found ${notebookId}`);
+      return false;
+    }
 
     // SIMULATION LOGIC
     // Simulate 10% chance service is DOWN
